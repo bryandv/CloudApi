7495365c0daafc8e04bf6f0e1427af49a9ff1f33
bryan <bryan_dv@hotmail.com>
2019-05-26 00:52:20 +0200

    SWAPI REST api toegevoegd

--- AngularPP3/AngularPP3/src/app/app.module.ts
+++ AngularPP3/AngularPP3/src/app/app.module.ts
@@ -3,16 +3,28 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http'; 
 import { AppComponent } from './app.component';
-
+import {RouterModule} from '@angular/router'
 import {ButtonModule} from 'primeng/button';
 import {TableModule} from 'primeng/table';
+import {TabMenuModule} from 'primeng/tabmenu';
+import {ToolbarModule} from 'primeng/toolbar';
+import {CardModule} from 'primeng/card';
+import {PanelModule} from 'primeng/panel';
+import {InputTextModule} from 'primeng/inputtext';
+import {FormsModule} from '@angular/forms'
 import { TeamsComponent } from './teams/teams.component';
+import { SwapiComponent } from './swapi/swapi.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TeamsComponent,
+    SwapiComponent,
+    NavbarComponent,
+    HomeComponent,
     
   ],
   imports: [
@@ -20,7 +32,20 @@ import { TeamsComponent } from './teams/teams.component';
     BrowserAnimationsModule,
     HttpClientModule,
     ButtonModule,
-    TableModule
+    TableModule,
+    TabMenuModule,
+    FormsModule,
+    CardModule,
+    InputTextModule,
+    PanelModule,
+    ToolbarModule,
+    RouterModule.forRoot([
+      { path: "", redirectTo: "home", pathMatch: "full"},
+      {path: "home",component:HomeComponent},
+      {path: "star",component:SwapiComponent},
+      {path: "pp",component:TeamsComponent}
+    ])
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
